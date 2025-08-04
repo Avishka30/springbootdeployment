fb840500a4494733fc06852ca0ee5d5c17b19244
Avishka <kavindua627@gmail.com>
2025-08-04 10:04:50 +0530

    update

--- src/main/java/lk/ijse/gdse/springbootdeployment/CustomerController.java
+++ src/main/java/lk/ijse/gdse/springbootdeployment/CustomerController.java
@@ -1,4 +1,15 @@
 package lk.ijse.gdse.springbootdeployment;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
+
+    @GetMapping
+    public String getCustomers() {
+        return "Hello World";
+    }
 }
